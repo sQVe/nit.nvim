02914f190d414011ce27b542fcd8052ffa5593ae
Oskar Grunning <oskargrunning@gmail.com>
2026-02-03 17:28:52 +0100

    feat: add user command and key mappings for PR review

--- plugin/nit.lua
+++ plugin/nit.lua
@@ -2,3 +2,21 @@ if vim.g.loaded_nit then
   return
 end
 vim.g.loaded_nit = true
+
+vim.api.nvim_create_user_command('Nit', function(opts)
+  require('nit.commands').dispatch(opts)
+end, {
+  nargs = '*',
+  complete = function(...)
+    return require('nit.commands').complete(...)
+  end,
+  desc = 'nit.nvim PR review',
+})
+
+vim.keymap.set('n', '<Plug>(nit-open)', function()
+  vim.notify('nit-open: not yet implemented', vim.log.levels.WARN)
+end, { desc = 'Open PR review' })
+
+vim.keymap.set('n', '<Plug>(nit-healthcheck)', function()
+  vim.cmd('checkhealth nit')
+end, { desc = 'Run nit health check' })
